a80bea57f32922f45a5d5ec80c3b9da8463aa17f
KittenDeveloper <fuller.justice@gmail.com>
2018-01-15 02:10:47 -0600

    C GUI Windows
    Wrote the roku scanning function.
    Various small changes.

--- C/GUI/Windows/RokuRemote.c
+++ C/GUI/Windows/RokuRemote.c
@@ -4,6 +4,36 @@
 #include <windows.h>
 #include <stdlib.h>
 #include <tchar.h>
+#include <string.h>
+#define STR_EXPAND(tok) #tok
+#define STR(tok) STR_EXPAND(tok)
+#define ERR_BOX MessageBoxA(NULL, STR(Error near line: __LINE__ Check error code for details), NULL, MB_OK); 
+char recvBuffer[1024]={'\0'};
+char *discoverRokus()
+{
+	int sockaddr_in_size=sizeof(struct sockaddr_in);
+	char ssdpMessage[]="M-SEARCH * HTTP/1.1\nHost: 239.255.255.250:1900\nMan: \"ssdp:discover\"\nST: roku:ecp\n\n";
+	WSADATA wsa;
+	SOCKET s;
+	if(WSAStartup(0x0101, &wsa)){ERR_BOX; exit(WSAGetLastError());}
+	if((s=socket(AF_INET, SOCK_DGRAM, 0))==INVALID_SOCKET){ERR_BOX; exit(WSAGetLastError());}
+	struct sockaddr_in server;
+	memset((void *)&server, '\0', sizeof(struct sockaddr_in));
+	server.sin_family=AF_INET;
+	server.sin_port=htons(1900);
+	server.sin_addr.S_un.S_addr= inet_addr("239.255.255.250");
+	if(sendto(s, ssdpMessage, (int)strlen(ssdpMessage)+1, 0, (struct sockaddr*)&server, sizeof(struct sockaddr_in))==-1)
+	{
+		ERR_BOX; exit(WSAGetLastError());
+	}
+	if(recvfrom(s, (char*)&recvBuffer, (int)sizeof(recvBuffer), 0, (struct sockaddr*)&server, &sockaddr_in_size)<0)
+	{
+		ERR_BOX; exit(WSAGetLastError());
+	}
+	closesocket(s);
+	WSACleanup();
+	return recvBuffer;
+}
 LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	PAINTSTRUCT ps;
@@ -25,6 +55,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 }
 int main(int argc, char **argv)
 {
+	printf("%s", discoverRokus());
 	TCHAR szWindowClass[]= _T("RokuRemote");
 	TCHAR szTitle[]= _T("Roku Remote");
 	WNDCLASSEX wc;
